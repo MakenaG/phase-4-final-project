ad06eb2eee3cbd47de5ae7c0a3308248f4ecf0a8
Gregory <elvismabisi@gmail.com>
2023-03-29 22:24:06 +0300

    added post request to signup

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,94 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import './Sign.css';
-
 function SignUp() {
-  // Initialize the form values and error messages as empty strings
-  const initialValues = { firstname: "", lastname:"", email: "", password: "" };
-  const [formValues, setFormValues] = useState(initialValues);
-  const [formErrors, setFormErrors] = useState({});
-
-  // Keep track of whether the form has been submitted
-  const [isSubmit, setIsSubmit] = useState(false);
-
-  // Handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Validate the form values and set the error messages
-    setFormErrors(validate(formValues));
-    // Set the "isSubmit" flag to true to indicate that the form has been submitted
-    setIsSubmit(true);
-  };
-
-  // Reset the form values to their initial state when there are no errors and the form has been submitted
-  useEffect(() => {
-    console.log(formErrors);
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      setFormValues(initialValues);
-    }
-  }, [formErrors]);
-
-  // Form validation function
-  const validate = (values) => {
-    const errors = {};
-    if (!values.firstname) {
-      errors.firstname = "Firstname is required!";
-    }
-    if (!values.lastname) {
-      errors.lastname = "Lastname is required!";
-    }
-    if (!values.email) {
-      errors.email = "Email is required!";
-    }
-    if (!values.password) {
-      errors.password = "password is required!";
-    } else if (values.password.length < 4) {
-      errors.password = "password must be more than 4 letters";
-    }
-    return errors;
-  };
-
-  // Handle form input changes and update the form values accordingly
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-    console.log(formValues);
-  };
-
-  // Styling for the form element
-  const formStyles = {
-    border: "1px solid skyblue",
-    padding: "10px",
-    borderRadius: "10px",
-  };
+    // Initialize the form values and error messages as empty strings
+    const initialValues = {username:"", email: "", password: "" };
+    const [formValues, setFormValues] = useState(initialValues);
+    const [formErrors, setFormErrors] = useState({});
+  
+    // Keep track of whether the form has been submitted
+    const [isSubmit, setIsSubmit] = useState(false);
+  
+    // Handle form submission
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      // Validate the form values and set the error messages
+      setFormErrors(validate(formValues));
+      // Set the "isSubmit" flag to true to indicate that the form has been submitted
+      setIsSubmit(true);
+  
+      // If there are no errors, submit the form data to the server
+      if (Object.keys(formErrors).length === 0) {
+        try {
+          const response = await fetch('/api/signup', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formValues),
+          });
+          const data = await response.json();
+          console.log(data);
+          // Optionally, reset the form to its initial state after the request has completed
+          setFormValues(initialValues);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+  
   return (
     <div className='form-container'>
         
         <form style={formStyles} onSubmit={handleSubmit}>
         <h3>Sign Up</h3>
         <div className="mb-3">
-          <label>First Name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="John"
-            name='firstname'
-            value={formValues.firstname}
-            onChange={handleChange}
-          />
-        </div>
-        <p>{formErrors.firstname}</p>
-        <div className="mb-3">
-          <label>Last Name</label>
+          <label>User Name</label>
           <input 
            type="text" 
            className="form-control" 
            placeholder="Doe" 
-           name='lastname'
-           value={formValues.lastname}
+           name='username'
+           value={formValues.username}
            onChange={handleChange}
            />
         </div>
-        <p>{formErrors.lastname}</p>
+        <p>{formErrors.username}</p>
         <div className="mb-3">
           <label>Email Address</label>
           <input
