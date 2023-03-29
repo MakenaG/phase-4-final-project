18def0ba86c0db51e900d22f55fd4093e701e137
MakenaG <makenaglory44@gmail.com>
2023-03-29 11:21:30 +0300

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import UploadVideo from './components/protected  pages/UploadVideo';
+import UserVideos from './components/protected  pages/UserVideos';
+import UserComments from './components/protected  pages/UserComments';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <>
+      <UploadVideo />
+      <UserVideos/>
+      <UserComments/>
       
+      </>
     </div>
   );
 }
