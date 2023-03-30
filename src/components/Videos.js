d7c1a1b7c7b7a8c3fc0d1626fda377db6d928411
grace_aloo <graceaaloo@gmail.com>
2023-03-30 22:51:39 +0300

    error handling

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -5,18 +5,43 @@ import { faHeart} from "@fortawesome/free-solid-svg-icons";
 
 
 function Videos(){
-    const [allVids,SetallVids] = useState([])
+    const [allVids,setAllVids] = useState([])
+    const [errors,setErrors] = useState([])
 
     useEffect(()=>{
         fetch(`https://backend-dc1w.onrender.com/all_videos`)
-        .then(res =>res.json())
-        .then(data =>SetallVids(data))
+        .then(res =>{
+          if(res.ok){
+            res.json().then((data)=>setAllVids(data))
+          }else{
+            res.json().then((err)=>setErrors(err.errors))
+          }
+        })
     },[])
-    console.log(allVids)
+    
+    function handleLike(id) {
+      fetch(`https://backend-dc1w.onrender.com/all_videos/${id}`, {
+        method: "POST",
+        headers: {
+              "Content-Type": "application/json"
+            },
+      })
+        .then((res) => {
+          if(res.ok){
+          res.json().then((data) => {
+            const updatedVids = allVids.map((vid) =>
+              vid.id === data.id ? { ...vid, likes: data.likes } : vid
+            );
+            setAllVids(updatedVids);
+          })
+        }else{
+          res.json().then((err)=>setErrors([err.errors]))
+        }})
 
-    function play(e){
-      e.target.querySelector('iframe').play()
-    }  
+    }
+
+
+    console.log(errors)
     return(
         <div className="mt-4"> 
         <Container>
@@ -29,7 +54,6 @@ function Videos(){
                   src={vid.video} 
                   poster={vid.image} 
                   title={vid.title}
-                  onMouseOver={()=>play}
                   autoPlay
                   muted
                   loop 
@@ -40,13 +64,27 @@ function Videos(){
                 <Card.Body>
                   <Card.Title>{vid.title}</Card.Title>
                   <Card.Text>{vid.description}</Card.Text>
-                  <Button variant="primary" className="me-2"><FontAwesomeIcon icon={faHeart}>Like</FontAwesomeIcon>Like</Button>
+                  <Button 
+                  variant="primary" 
+                  className="me-2"
+                  onClick={() => handleLike(vid.id)}
+                  >
+                    <FontAwesomeIcon icon={faHeart}>
+                    {vid.likes} Likes </FontAwesomeIcon>Like {vid.likes}</Button>
                   <Button variant="primary">Watch now</Button>
+                  {errors.length > 0 && (
+                    <div className="text-danger mt-3">
+                      {errors.map((error, index) => (
+                        <p key={index}>{error}</p>
+                      ))}
+                    </div>
+                  )}
                 </Card.Body>
               </Card>
             </Col>
           ))}
         </Row>
+        
       </Container>
       </div>
     )
