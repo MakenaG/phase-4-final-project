725472a21677f7525c874aba31d3e9624647be4c
Black-Male <Gkaranja20@gmail.com>
2023-03-31 17:27:58 +0300

    Done with my front end parts, left with routing

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Profile from './components/protected  pages/Profile';
-import FaveVids from './components/protected  pages/FavouriteVideo';
-
-
-
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <FaveVids/>
+      <Profile/>
 
     </div>
   );
