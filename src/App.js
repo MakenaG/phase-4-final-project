15326526f4f89f82ab1d0e82826a44abd26708f3
Gregory <elvismabisi@gmail.com>
2023-03-31 09:59:22 +0300

    new changes to login and register

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
+import React, {useState} from 'react';
 import UploadVideo from './components/protected  pages/UploadVideo';
 import UserVideos from './components/protected  pages/UserVideos';
 import UserComments from './components/protected  pages/UserComments';
 import './App.css';
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
+import { Login } from './components/Login';
+import { Register } from './components/Register';
 
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login')
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName)
+  }
   return (
     <div className="App">
-      <Navbar/>
-      <LandingPage/>
-      <UploadVideo />
-      <UserVideos/>
-      <UserComments/>
+    {
+      currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
+    }
+      
     </div>
   );
 }
